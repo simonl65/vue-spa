d755895d9662834d4747f80f69b40c69bea32674
Simon Lincoln <simon.lincoln@contentdiscovered.com>
2018-10-30 15:15:40 +0000

    video:2 @6:30

---
+++ resources/js/components/MainApp.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card card-default">
+          <div class="card-header">Main App</div>
+
+          <div class="card-body">
+            This is our main app
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "main-app"
+};
+</script>
